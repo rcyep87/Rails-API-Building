788c1191a6a2643885220d67ff8640770da1cf8b
Ryan Yep <ryan.yep@gmail.com>
2015-09-17 16:32:51 -0500

    wrote database info using Faker (before seeding)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
   has_many :orders
-  has_many :items, :through :orders
+  has_many :items, through: :orders
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Create 15 users
+# 15 unique addresses
+# 25 unique items
+# constraint of 1-25 item_id for Order
+# constraint of 1-15 user_id for Order
+
+15.times do
+  user = User.create({
+              id: Faker::Number.between(1, 15)
+      first_name: Faker::Name.first_name,
+       last_name: Faker::Name.last_name,
+             age: Faker::Number.between(15, 45) })
+
+  Address.create({
+        user_id: user.id
+     street_name: Faker::Address.street_name
+            city: Faker::Address.city
+           state: Faker::Address.state
+             zip: Faker::Address.zip })
+    25.times do
+      item = Item.create({
+                    id: Faker::Number.between(1, 25)
+                  name: Faker::Team.creature
+                 price: rand(100000)
+           description: Faker::Team.creature })
+
+      Order.create({
+               item_id: item.id
+               user_id: user.id
+              quantity: rand(1000)
+          })
+end
+
+
