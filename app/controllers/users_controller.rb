caca0bd5355a67b4cedd69e77d9c0306dccd7bb4
Ryan Yep <ryan.yep@gmail.com>
2015-09-18 09:56:59 -0500

    Can successfully create new user and throws error for missing information.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,10 +12,15 @@ class UsersController < ApplicationController
     else
       render json: { error_msg: "Record not found!", id: params[:id] }.to_json, status: 404
     end
-
   end
 
   def create
+    if params[:first_name].empty? || params[:last_name].empty? || params[:age].empty?
+      render json: { error_msg: "You've provided incomplete information" }.to_json, status: 404
+    else
+      user = User.create({ first_name: params[:first_name], last_name: params[:last_name], age: params[:age] })
+      render json: user.to_json, status: 200
+    end
   end
 
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 
-  get '/orders'     , to: 'orders#index'
-  get '/users'      , to: 'users#index'
-  get '/users/:id'  , to: 'users#show'
-  get '/addresses'  , to: 'addresses#index'
-  get '/items'      , to: 'items#index'
+  get   '/orders'     , to: 'orders#index'
+
+  get   '/users'      , to: 'users#index'
+  get   '/users/:id'  , to: 'users#show'
+  post  '/users'      , to: 'users#create'
+
+  get   '/addresses'  , to: 'addresses#index'
+  get   '/items'      , to: 'items#index'
 
 end
