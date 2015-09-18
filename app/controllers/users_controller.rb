b53fad427ccefaf64f37b941f593a6732d29825b
Ryan Yep <ryan.yep@gmail.com>
2015-09-18 09:29:06 -0500

    displays single user, if not found displays error message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,13 @@ class UsersController < ApplicationController
   end
 
   def show
+    if User.exists?(params[:id])
+      user = User.find(params[:id])
+      render json: user.to_json, status: 200
+    else
+      render json: { error_msg: "Record not found!", id: params[:id] }.to_json, status: 404
+    end
+
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   get '/orders'     , to: 'orders#index'
   get '/users'      , to: 'users#index'
+  get '/users/:id'  , to: 'users#show'
   get '/addresses'  , to: 'addresses#index'
   get '/items'      , to: 'items#index'
 
