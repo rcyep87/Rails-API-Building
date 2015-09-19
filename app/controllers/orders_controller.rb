d59d2256fca41b0e69b9b07b1e494923613f9b3d
Ryan Yep <ryan.yep@gmail.com>
2015-09-19 11:29:23 -0500

    Displays single order by primary key; if primary key is not found in system throws error

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,8 +6,12 @@ class OrdersController < ApplicationController
   end
 
   def show
-    order = Order.find(params[:id])
-    render json: order.to_json, status: 200
+    if Order.exists?(params[:id])
+      order = Order.find(params[:id])
+      render json: order.to_json, status: 200
+    else
+      render json: { error_msg: "Unable to locate order with ID:#{params[:id]} in system!" }.to_json, status: 404
+    end
   end
 
   def create
